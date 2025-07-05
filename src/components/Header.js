9f750942057f3b7963e215cfd0d6387529dadabe
Travis McGray <tmcgray204@gmail.com>
2025-07-05 14:23:15 -0400

    The App Component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Navbar, NavbardBrand } from 'reactstrap';
+import NucampLogo from '../app/assets/img/logo.png';
+
+const Header = () => {
+    return ( 
+        null
+    );
+};
+
+export default Header
