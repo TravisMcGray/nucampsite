c9039dd9a0fdec27aa6811b0ae93029bab894a41
Travis McGray <tmcgray204@gmail.com>
2025-07-09 14:27:51 -0400

    campsitesSlice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import { Navbar, NavbardBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
     return ( 
-        null
+       <Navbar dark color='primary' sticky='top' expand='md'>
+            <NavbarBrand href='/'>
+                <img src={NucampLogo} alt='nucamp logo' />
+            </NavbarBrand>
+        </Navbar>
     );
 };
 
-export default Header
+export default Header;
