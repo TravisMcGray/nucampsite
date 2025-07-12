fef68a291f944f2b3c8e0a9a70653aa1ad84b2bb
Travis McGray <tmcgray204@gmail.com>
2025-07-12 15:13:30 -0400

    Partners & PartnersList

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -2,7 +2,7 @@ import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
-}
+};
 
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
