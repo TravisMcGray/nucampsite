9e9297260e056deb4deecaab58403d571b9e5e9c
Travis McGray <tmcgray204@gmail.com>
2025-08-01 22:06:40 -0400

    Async DisplayList

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -5,6 +5,8 @@ import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { useSelector } from 'react-redux';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const DisplayList = () => {
     const items = useSelector((state) => [
@@ -15,12 +17,21 @@ const DisplayList = () => {
 
     console.log('display items:', items);
     return (
-        <Row>
+            <Row>
             {items.map((item, idx) => {
+                const { featuredItem, isLoading, errMsg } = item;
+                if (isLoading) {
+                    return <Loading key={idx} />;
+                }
+                if (errMsg) {
+                    return <Error errMsg={errMsg} key={idx} />;
+                }
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <AnimatedDisplayCard item={item} />
-                    </Col>
+                    featuredItem && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={featuredItem} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
