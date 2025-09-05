54579c493a6d07271d940cda9f5aba2bd98d8cb1
Travis McGray <tmcgray204@gmail.com>
2025-09-05 13:44:48 -0400

    Sending Email

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,17 @@
 import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 const ContactScreen = () => {
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp,co'],
+            subject: 'Informationuiry',
+            body: 'To whome it may concern'
+        });
+    };
+
     return (
         <ScrollView>
             <Animatable.View
@@ -16,6 +25,19 @@ const ContactScreen = () => {
                     </Card.Title>
                     <Card.Divider />
                     <Text>1 Nucamp</Text>
+                    <Button
+                        title='Send email'
+                        buttonStyle={{ backgroundColor: '#4637DD', margin: 40 }}
+                        icon={
+                            <Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{ marginRight:10}}
+                            />
+                        }
+                        onPress={() => sendMail()}
+                    />
                     <Text>Seattle, WA 98001</Text>
                     <Text style={{ marginBottom: 10 }}>U.S.A</Text>
                     <Text>Phone: 1-206-555-1234</Text>
