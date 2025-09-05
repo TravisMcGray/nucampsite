266fa3679c49d83265afc93cf0a865cce069a3e2
Travis McGray <tmcgray204@gmail.com>
2025-09-05 13:30:39 -0400

    Local Nofitications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, Switch, Button, Platform, Alert, StyleSheet } f
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -17,16 +18,33 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
         Alert.alert(
-            'Your Reservation',
-            `Number of Campers: ${campers}\nHike In?: ${hikeIn ? 'Yes' : 'No'}\nDate: ${date.toLocaleDateString('en-US')}`,
+            'Begin Search?',
+            message,
             [
-                { text: 'Cancel', onPress: resetForm, style: 'cancel' },
-                { text: 'OK', onPress: resetForm }
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
+                }
             ],
             { cancelable: false }
         );
-
         console.log('campers:', campers);
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
@@ -36,34 +54,58 @@ const ReservationScreen = () => {
         setCampers(1);
         setHikeIn(false);
         setDate(new Date());
-        setShowCalendar(false)
+        setShowCalendar(false);
+    };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     };
 
     return (
         <ScrollView>
-            <Animatable.View
-                animation='zoomIn'
-                duration={2000}
-                delay={1000}
-                >
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
-                    <Picker 
+                    <Picker
                         style={styles.formItem}
                         selectedValue={campers}
                         onValueChange={(itemValue) => setCampers(itemValue)}
                     >
-                        <Picker.Item label ='1' value={1} />
-                        <Picker.Item label ='2' value={2} />
-                        <Picker.Item label ='3' value={3} />
-                        <Picker.Item label ='4' value={4} />
-                        <Picker.Item label ='5' value={5} />
-                        <Picker.Item label ='6' value={6} />
+                        <Picker.Item label='1' value={1} />
+                        <Picker.Item label='2' value={2} />
+                        <Picker.Item label='3' value={3} />
+                        <Picker.Item label='4' value={4} />
+                        <Picker.Item label='5' value={5} />
+                        <Picker.Item label='6' value={6} />
                     </Picker>
                 </View>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Hike In?</Text>
-                    <Switch 
+                    <Switch
                         style={styles.formItem}
                         value={hikeIn}
                         trackColor={{ true: '#5637DD', false: null }}
@@ -71,14 +113,13 @@ const ReservationScreen = () => {
                     />
                 </View>
                 <View style={styles.formRow}>
-                    <Text>
-                        <Button
-                            onPress={() => setShowCalendar(!showCalendar)}
-                            title={date.toLocaleDateString('en-US')}
-                            color='#5637DD'
-                            accessibilityLabel='Tape me to select Reservation date'
-                        />
-                    </Text>
+                    <Text style={styles.formLabel}>Date:</Text>
+                    <Button
+                        onPress={() => setShowCalendar(!showCalendar)}
+                        title={date.toLocaleDateString('en-US')}
+                        color='#5637DD'
+                        accessibilityLabel='Tap me to select a reservation date'
+                    />
                 </View>
                 {showCalendar && (
                     <DateTimePicker
@@ -93,13 +134,13 @@ const ReservationScreen = () => {
                     <Button
                         onPress={() => handleReservation()}
                         title='Search Availability'
-                        color='#56377D'
+                        color='#5637DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
             </Animatable.View>
         </ScrollView>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
@@ -118,6 +159,5 @@ const styles = StyleSheet.create({
         flex: 1
     }
 });
-
 
 export default ReservationScreen;
